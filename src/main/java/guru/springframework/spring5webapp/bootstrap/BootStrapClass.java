d8e6a1fa6abb803d27b9975cb6c6af889e2e773b
Yana Tereshchenko <yana.tereshchenko@softpos.eu>
2022-07-01 11:39:22 +0300

    Added Publisher Entity

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapClass.java
@@ -0,0 +1,36 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapClass implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private  final BookRepository bookRepository;
+
+    public BootStrapClass(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric","Evans");
+        //Publisher publisher = new Publisher("Address Line 1","Any City","Any State","Any Zip");
+        Book ddd = new Book("Some book","some isbn"/*,publisher*/);
+        ddd.getAuthors().add(eric);
+        eric.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        System.out.println("Started in bootstrap");
+        System.out.println("Number of books " + bookRepository.count());
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,96 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String tltle;
+    private String isbn;
+    @ManyToOne
+    private Publisher publisher;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String tltle, String isbn/*, Set<Author> authors, Publisher publisher*/) {
+        this.tltle = tltle;
+        this.isbn = isbn;
+        //this.authors = authors;
+        //this.publisher = publisher;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTltle() {
+        return tltle;
+    }
+
+    public void setTltle(String tltle) {
+        this.tltle = tltle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", tltle='" + tltle + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
